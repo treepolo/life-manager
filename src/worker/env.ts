73e43fe314defdacce81d131aa110bc5918c41f4
treepolo <bigtreepolo@gmail.com>
2026-08-30 07:20:58 +0800

    refactor: simplify worker environment bindings

--- src/worker/env.ts
+++ src/worker/env.ts
@@ -6,22 +6,4 @@ export interface Env {
   ACCESS_TEAM_DOMAIN?: string;
   ACCESS_AUD?: string;
   ACCESS_ALLOWED_EMAIL?: string;
-  TOKEN_ENCRYPTION_KEY?: string;
-  GOOGLE_CLIENT_ID?: string;
-  GOOGLE_CLIENT_SECRET?: string;
-  META_CLIENT_ID?: string;
-  META_CLIENT_SECRET?: string;
-  INSTAGRAM_API_VERSION: string;
-  PROVIDER_SYNC_INTERVAL_HOURS?: string;
-  OPERATION_LOG_RETENTION_DAYS?: string;
-  NOTIFICATION_LOG_RETENTION_DAYS?: string;
-  OAUTH_STATE_RETENTION_DAYS?: string;
-  OAUTH_STATE_TTL_MINUTES: string;
-  OAUTH_CALLBACK_BASE_URL?: string;
-  RESEND_API_KEY?: string;
-  RESEND_FROM?: string;
-  RESEND_TO?: string;
-  WEB_PUSH_VAPID_PUBLIC_KEY?: string;
-  WEB_PUSH_VAPID_PRIVATE_KEY?: string;
-  WEB_PUSH_VAPID_SUBJECT?: string;
 }
