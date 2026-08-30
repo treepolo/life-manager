ee2939ca394b4cf8c6db62dd6716901a6955a6b7
treepolo <bigtreepolo@gmail.com>
2026-08-30 17:34:54 +0800

    fix: ignore local maintenance artifacts in lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,16 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules", "playwright-report", ".wrangler"] },
+  {
+    ignores: [
+      "dist",
+      "node_modules",
+      "playwright-report",
+      ".wrangler",
+      ".tmp",
+      "backups"
+    ]
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
